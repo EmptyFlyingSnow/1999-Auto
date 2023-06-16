97459cc9a0edd17ce643d0da7ac2f95d892be44d
EmptyFlyingSnow <87599045+EmptyFlyingSnow@users.noreply.github.com>
2023-06-17 03:16:07 +0800

    增加了若干注释（暂未全部完成）

--- decisions/decision_1.py
+++ decisions/decision_1.py
@@ -54,15 +54,15 @@ def use(t:Turn,id:int):
     return (click,wait,f'合成 {hec}')
 
 def find_best(t:Turn):
-    b = -114514
+    b = -114514#啊这……
     ret = 0
     # print(t.data)
-    for i in range(0,7):
+    for i in range(0,7):#遍历权重，找出最大值
         if t.data[i] > b:
             b = t.data[i]
             ret = i
     print(f"选择使用{t.card[ret][0]} star : {t.card[ret][1]} at {ret}")
-    if t.card[ret][0] in cards_arrow.buff:
+    if t.card[ret][0] in cards_arrow.buff:#如果是对应效果卡，重置周期（处理略显草率，不具有普适性，但简单能用）
         t.buff = 2
     if t.card[ret][0] in cards_arrow.debuff:
         t.debuff = 2
@@ -72,29 +72,29 @@ def find_best(t:Turn):
     
 
 def pri_test(t:Turn):
-    t.data = [0,0,0,0,0,0,0,0]
+    t.data = [0,0,0,0,0,0,0,0]#某些情况下会有八张手牌
     for i in range(0,7):
         if t.card[i][0] == '无卡牌':
             t.data[i] -= 10000
-    for i in range(0,7):
-        for j in range(i+1,7):
-            if is_same(t,i,j) and t.card[i][1] < 3:
-                value = 1
-                if t.card[i][0] in cards_arrow.dps_aoe:
+    for i in range(0,7):#遍历手牌
+        for j in range(i+1,7):#当前比较手牌为j和i
+            if is_same(t,i,j) and t.card[i][1] < 3:#如果是相似卡且星级小于3
+                value = 1#权值设为1
+                if t.card[i][0] in cards_arrow.dps_aoe:#如果是高输出aoe卡牌
                     value *= 1.2
-                if t.card[i][0] in cards_arrow.aoe:
+                if t.card[i][0] in cards_arrow.aoe:#如果是aoe卡
                     value *= 1.1
-                if t.card[i][0] in cards_arrow.buff:
+                if t.card[i][0] in cards_arrow.buff:#如果是buff卡
                     value *= 1.5
-                for k in range(i+1,j):
-                    t.data[k] += int(((7-j+i)*(7-j+i)) * value)
+                for k in range(i+1,j):#遍历i和j之间的卡牌
+                    t.data[k] += int(((7-j+i)*(7-j+i)) * value)#增加其间的手牌权值，i和j的差值越小，增加的越多（吧？）
     for i in range(0,7):
-        if t.card[i][0] in cards_arrow.limit_aoe:
+        if t.card[i][0] in cards_arrow.limit_aoe:#以下部分，分别根据卡牌的属性予以适当权值调整
                 t.data[i] += 25
     for i in range(0,7):
         if t.card[i][0] in cards_arrow.limit_one:
                 t.data[i] += 23
-    for i in range(0,7):
+    for i in range(0,7):#三星卡
         if t.card[i][1] == 3:
                 t.data[i] += 6
     for i in range(0,7):
@@ -102,13 +102,13 @@ def pri_test(t:Turn):
                 t.data[i] += 3
     for i in range(0,7):
         if t.card[i][0] in cards_arrow.buff:
-            if t.buff <= 0:
+            if t.buff <= 0:#如果buff周期<1，即现在没有buff
                 t.data[i] += 50
             else:
                 t.data[i] -= 10
     for i in range(0,7):
         if t.card[i][0] in cards_arrow.debuff:
-            if t.debuff <= 0:
+            if t.debuff <= 0:#如果debuff周期<1，即现在没有debuff
                 t.data[i] += 50
             else:
                 t.data[i] -= 10
@@ -129,11 +129,11 @@ def pri_test(t:Turn):
                 t.data[i] -= 5
 
 def normal_cards_upgrade(t:Turn):
-    pri_test(t)
+    pri_test(t)#计算手牌权重
     print(t.card)
     print(t.data)
     a1 = use(t,find_best(t))
-    pri_test(t)
+    pri_test(t)#打出手牌后重新计算权重
     print(t.card)
     print(t.data)
     a2 = use(t,find_best(t))
@@ -141,7 +141,7 @@ def normal_cards_upgrade(t:Turn):
     print(t.card)
     print(t.data)
     a3 = use(t,find_best(t))
-    return (a1,a2,a3)
+    return (a1,a2,a3)#返回使用的三张手牌
 
 
 
